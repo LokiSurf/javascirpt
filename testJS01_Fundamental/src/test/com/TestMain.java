c36f355ef31428e082f77cbccff0fdb3bc62f79e
yangssem <rf00000@naver.com>
2014-08-05 14:52:06 +0900

    index.html

---
+++ testJS01_Fundamental/src/test/com/TestMain.java
@@ -0,0 +1,15 @@
+package test.com;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		int x;
+//		if(true){
+//			int x = 100;
+//		}
+//		
+//		System.out.println(x);
+		
+	}
+
+}
